abca5b864308017fce096373da8af4d78feb54b9
Joseph Abraham <ekurubal24@gmail.com>
2020-10-12 14:16:44 +0300

    news file initilazed

--- NewsPage/src/News.js
+++ NewsPage/src/News.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import {} from 'react-native';
+import { SafeAreaView, View, Text, ScrollView } from 'react-native';
 
+const myArray = Array.from(Array(50).keys());
 const News = () => {
-    return null;
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <ScrollView style ={{flex:1}}>
+                { 
+                myArray.map(u => <Text style={{fontsize:50}}>{u}</Text>)
+                }
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default News;
